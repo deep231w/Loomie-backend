4e5de97697b9b9d1adde04b4b010a5d433d1cdd5
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-10-23 03:06:14 +0530

    cocket added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import cors from "cors";
 import dotenv from "dotenv";
 import userRoute from './route/userRoutes/userRoute.js';
 import roomRoutes from './route/room/room.js';
+import socketHandler from "./socket.js";
+
+import http from 'http';
+import {Server} from 'socket.io';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//
+const server= http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST"],
+  },
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +38,8 @@ app.get("/", (req, res) => {
   res.send("Loomie backend is running!");
 });
 
-app.listen(PORT, () => {
+socketHandler(io);
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ src/socket.js
@@ -0,0 +1,48 @@
+
+export default function socketHandler(io) {
+
+    const roomState={}
+
+    io.on("connection", (socket) => {
+        console.log("⚡New user connected:", socket.id);
+
+        socket.on('join_room',({roomId})=>{
+            if(!roomId) return;
+
+            socket.join(roomId)
+            console.log(`socket id ${socket.id} joined the room ${roomId}`);
+
+            if(!roomState[roomId]){
+                roomState[roomId]={
+                    videoId: "dQw4w9WgXcQ",
+                    currentTime: 0,
+                    isPlaying: false,
+                    lastUpdate: Date.now()
+                }
+            }
+
+            // Send current state to the newly joined user
+            const state = roomState[roomId];
+            socket.emit("sync_state", state);
+
+        })
+
+        socket.on("video_action", ({ roomId, action, currentTime }) => {
+            if (!roomId || !roomState[roomId]) return;
+
+            const state = roomState[roomId];
+
+            state.currentTime = currentTime;
+            state.isPlaying = action === "play" ? true : action === "pause" ? false : state.isPlaying;
+            state.lastUpdate = Date.now();
+
+            socket.to(roomId).emit("video_action", { action, currentTime });
+        });
+
+
+
+        socket.on("disconnect", () => {
+            console.log("❌ User disconnected:", socket.id);
+        });
+  });
+}
